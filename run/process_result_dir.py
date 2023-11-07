8f731b9be56a555aa24887cd780901629cb5d36a
jingliu9 <jingliu.cs@gmail.com>
2023-11-07 09:19:26 -0700

    del

--- run/process_result_dir.py
+++ run/process_result_dir.py
@@ -10,7 +10,7 @@ results_dict = dict()
 results_dict[f'total_time_ns'] = list()
 results_dict['fault_op_num'] = list()
 results_dict['recovery_ns'] = list()
-results_dict['save_ns'] = list()
+results_dict['checkpoint_ns'] = list()
 results_dict['restart_ns'] = list()
 
 for op_num in range(499, 48001, 499):
@@ -27,13 +27,17 @@ for op_num in range(499, 48001, 499):
         for line in lines:
             split = line.split()
             if len(split) > 3 and split[0] == "Timer" and split[1] == "0:":
-                results_dict["save_ns"].append(int(split[2])*1000)
+                # results_dict["save_ns"].append(int(split[2])*1000)
+                recovery_time += int(split[2])*1000
             if len(split) > 3 and split[0] == "Timer" and split[1] == "1:":
                 recovery_time += int(split[2])*1000
             if len(split) > 3 and split[0] == "Timer" and split[1] == "2:":
                 recovery_time += int(split[2])*1000
             if len(split) > 3 and split[0] == "Timer" and split[1] == "3:":
                 results_dict["restart_ns"].append(int(split[2])*1000)
+            if len(split) > 3 and split[0] == "Timer" and split[1] == "4:":
+                if '-nan' not in line:
+                    results_dict["checkpoint_ns"].append(int(split[2])*1000)
         results_dict["recovery_ns"].append(recovery_time)
 
 
