bfd90259d44e0b286925804ad8aa7355e7df0926
jingliu9 <jingliu.cs@gmail.com>
2023-11-23 18:45:25 -0700

    + run_crc_perf_gc

---
+++ run/run_crc_perf_gc.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <case_name>"
+    exit 1
+fi
+
+case_name=$1
+
+# Note: remember to source env
+DIR=crc_perf_inmem
+mkdir -p $DIR
+
+ARGS=""
+
+CONFIG_NAME="$DIR/$case_name.config"
+
+echo "CFS_FT_LIB_MEM_SIZE_KB:$CFS_FT_LIB_MEM_SIZE_KB" >> "${CONFIG_NAME}"
+echo "=======" >> "${CONFIG_NAME}"
+git log -1 >>"${CONFIG_NAME}"
+echo "=======" >> "${CONFIG_NAME}"
+TS_STR="$(date +%m_%d_%H-%M-%S)"
+git diff >"$DIR/${case_name}.${TS_STR}.diff"
+
+
+#IS_FAULT=0
+for NUM_SLOT_GC in "16384" "32768" "65536" "0"
+do
+    cur_case_name="${case_name}_GC${NUM_SLOT_GC}NumSlot"
+    ARGS=""
+    for ITER in {1..15}
+    do
+        sudo rm -rf /dev/shm/*
+        sleep 10
+        sudo -E python run_cpdir_gc.py $DIR "$cur_case_name$ITER" $NUM_SLOT_GC
+        ARGS="$ARGS $DIR/$cur_case_name$ITER-app.timer"
+    done
+    echo $ARGS
+    python crc_perf_avg.py $ARGS | tee "$DIR/$cur_case_name.avg"
+done
+
+
